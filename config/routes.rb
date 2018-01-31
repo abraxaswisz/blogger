10f93efad5da295eb61ff4a620f0ebfc94060adc
bartosz <abraxaswisz@gmail.com>
2018-01-31 20:47:22 +0100

    Login is now possible

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 	resources :articles do 
 		resources :comments
 	end
+	resources :author_sessions, only: [:new, :create, :destroy]
+	get 'login' => 'author_sessions#new'
+	get 'logout' => 'author_sessions#destroy'
 	root 'articles#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
