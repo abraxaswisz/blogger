3ae9e96e01fa0c61682adc9a1b1b7a5933f89537
bartosz <abraxaswisz@gmail.com>
2018-02-05 22:37:35 +0100

    About Me page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
 	resources :authors
 	resources :tags
 	resources :articles do 
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
 	get 'login' => 'author_sessions#new'
 	get 'logout' => 'author_sessions#destroy'
 	root 'articles#index'
+
+	get 'about' => 'about#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
